4b89f8291a85c3636bd1d28a9d145f609ba4cc4f
unknown <yuujin00_20@naver.com>
2025-03-26 16:54:54 +0900

    with sqlite

--- flask01/app.py
+++ flask01/app.py
@@ -1,9 +1,22 @@
 # flask run --debug --port 5001
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+import config
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():
     # 입구 파일을 하나 만들어줍니다.
     app = Flask(__name__)
+    app.config.from_object(config)
+
+    # ORM을 적용
+    db.init_app(app)
+    migrate.init_app(app,db)
+    
 
     from views import main_views, board_views
     app.register_blueprint(main_views.mbp)
